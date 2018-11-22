2802d31743190896ca8a4bc8479038091fe1d35e
Jimmy Situ <web@jimmystone.cn>
2018-11-22 23:53:08 +0800

    update miz701n board

--- xilinx/common/tcl/boards.tcl
+++ xilinx/common/tcl/boards.tcl
@@ -3,6 +3,7 @@
 namespace eval ::program::boards {}
 
 set ::program::boards::spec [dict create \
+	miz701n         [dict create  iface spix4   size 32   bitaddr 0x0        memdev {s25fl256sxxxxxx0-spi-x1_x2_x4}] \
 	arty            [dict create  iface spix4   size 16   bitaddr 0x0        memdev {n25q128-3.3v-spi-x1_x2_x4}] \
 	arty_a7_100     [dict create  iface spix4   size 16   bitaddr 0x0        memdev {s25fl128sxxxxxx0-spi-x1_x2_x4}] \
 	vc707           [dict create  iface bpix16  size 128  bitaddr 0x3000000  ] \
--- xilinx/miz701n/tcl/board.tcl
+++ xilinx/miz701n/tcl/board.tcl
@@ -1,4 +1,5 @@
 # See LICENSE for license details.
 set name {miz701n}
+set part_board {}
 set part_fpga {xc7z020clg400-2}
 set bootrom_inst {rom}
